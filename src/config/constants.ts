cf6d2f741799e36f578ee049917763d8d8e0e251
reginpv <reginpv@gmail.com>
2025-10-28 11:58:52 +0800

    add project name

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,10 +1,10 @@
-export const APP_NAME = 'NextCrud'
+export const APP_NAME = 'On-lap'
 export const APP_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://nextcrud.vercel.app'
+    ? 'https://on-lap.vercel.app/'
     : 'http://localhost:3000'
 
-export const SCHOOL_NAME = 'NextCrud'
+export const SCHOOL_NAME = 'On-lap'
 
-export const SMTP_FROM_NAME = 'NextCrud'
+export const SMTP_FROM_NAME = 'On-lap'
 export const SMTP_FROM_EMAIL = 'hello@domain.com'
